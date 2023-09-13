94a1a5c78f368073c1ac669615fc3bb6cf125f3a
shizu <616940+sntulix@users.noreply.github.com>
2023-09-13 09:48:09 +0900

    Create export_blosm_objects2gltf_separated.py

---
+++ export_blosm_objects2gltf_separated.py
@@ -0,0 +1,28 @@
+"""
+export_blosm_objects2gltf_separated.py.
+This script exports the city map loaded in Blosm as individual objects to gltf.
+And create modelfile list.
+Date: 20230913
+"""
+
+import os
+
+save_dir = "c:\\models\\"
+infofile_path = save_dir+"modellist.tsv"
+
+def export_active_object2glb(save_dir, infofile_path, obj):
+	obj.select_set(True)
+	save_filename = obj.name+".glb"
+	save_path = save_dir + save_filename
+	print(save_filename)
+	rot = obj.rotation_euler
+	f = open(infofile_path, "a")
+	f.write(obj.name + "\t" + str(obj.location[0]) + "\t" + str(obj.location[1]) + "\t" + str(obj.location[2]) + "\t" + str(degrees(rot[0])) + "\t" + str(degrees(rot[1])) + "\t" + str(degrees(rot[2])) + "\t" + str(obj.scale[0]) + "\t" + str(obj.scale[1]) + "\t" + str(obj.scale[2]) + "\n")
+	bpy.ops.export_scene.gltf(filepath=save_path, export_current_frame=True, export_format='GLTF_EMBEDDED', export_image_format='JPEG', export_jpeg_quality=80, use_selection=True)
+	f.close()
+
+os.remove(infofile_path)
+for o in bpy.context.scene.objects:
+	if o.type == 'MESH':
+		bpy.ops.object.select_all(action='DESELECT')
+		export_active_object2glb(save_dir, infofile_path, o)
